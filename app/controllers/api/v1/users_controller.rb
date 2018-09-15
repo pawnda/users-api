91f86f300b6d22238711394abf08ccbbbe348ecb
Georgy Shabunin <georgy.shabunin@gmail.com>
2018-09-15 12:55:05 +0200

    Add remaining user endpoints

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,18 +2,48 @@ module Api
   module V1
     class UsersController < ApplicationController
       before_action :authenticate_user
+      before_action :set_user, only: [:show, :update, :destroy]
 
-      # GET /users
       def index
         @users = User.all
 
         render json: @users
       end
 
+      def show
+        render json: @user
+      end
+
+      def create
+        @user = User.new(user_params)
+
+        if @user.save
+          render json: @user, status: :created
+        else
+          render json: @user.errors, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @user.update(user_params)
+          render json: @user
+        else
+          render json: @user.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @user.destroy
+      end
+
       private
 
+      def set_user
+        @user = User.find(params[:id])
+      end
+
       def user_params
-        params.require(:user).permit(:phone_number, :password, :password_confirmation)
+        params.require(:user).permit(:name, :phone_number, :password, :password_confirmation)
       end
     end
   end
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Users' do
   let(:users) { FactoryBot.create_list(:user, 10) }
+  let(:valid_attributes) { FactoryBot.attributes_for(:user) }
+  let(:invalid_attributes) { FactoryBot.attributes_for(:user, phone_number: nil) }
 
   describe 'GET /api/v1/users' do
     before(:each) do
@@ -18,6 +20,131 @@ RSpec.describe 'Users' do
     context 'with unauthenticated user' do
       before(:each) do
         get api_v1_users_path
+      end
+
+      it { expect(response).to have_http_status(:unauthorized) }
+    end
+  end
+
+  describe 'GET /api/v1/users/:user_id' do
+    before(:each) do
+      get api_v1_user_path(users.first.id), headers: auth_headers(users.first.id)
+    end
+
+    it 'should return the expected user attributes' do
+      user_response = json_response[:data]
+
+      expect(user_response[:id]).to eql users.first.id.to_s
+      expect(user_response[:attributes]['phone-number'.to_sym]).to eql users.first.phone_number
+      expect(user_response[:attributes]['name'.to_sym]).to eql users.first.name
+    end
+
+    it { expect(response).to have_http_status(:ok) }
+
+    context 'with unauthenticated user' do
+      before(:each) do
+        get api_v1_user_path(users.first.id)
+      end
+
+      it { expect(response).to have_http_status(:unauthorized) }
+    end
+  end
+
+  describe 'POST /api/v1/users' do
+    context 'with valid attributes' do
+      before(:each) do
+        post api_v1_users_path,
+            headers: auth_headers(users.first.id),
+            params: { user: valid_attributes }
+      end
+
+      it 'should return the new user params' do
+        user_response = json_response[:data][:attributes]
+        expect(user_response[:name]).to eql valid_attributes[:name]
+        expect(user_response['phone-number']).to eql valid_attributes['phone-number']
+      end
+
+      it { expect(response).to have_http_status(:created) }
+
+      context 'with unauthenticated user' do
+        before(:each) do
+          post api_v1_users_path, params: { user: valid_attributes }
+        end
+
+        it { expect(response).to have_http_status(:unauthorized) }
+      end
+    end
+
+    context 'with invalid attributes' do
+      before(:each) do
+        post api_v1_users_path,
+             headers: auth_headers(users.first.id),
+             params: { user: invalid_attributes }
+      end
+
+      it 'response contains the correct error message' do
+        error_response = json_response[:phone_number]
+
+        expect(error_response).to include "can't be blank"
+      end
+
+      it { expect(response).to have_http_status(:unprocessable_entity) }
+    end
+  end
+
+  describe 'PUT /api/v1/users/:user_id' do
+    context 'with valid attributes' do
+      before(:each) do
+        put api_v1_user_path(users.first.id),
+            headers: auth_headers(users.first.id),
+            params: { user: valid_attributes }
+      end
+
+      it 'should return the updated user params' do
+        user_response = json_response[:data][:attributes]
+        expect(user_response[:name]).to eql valid_attributes[:name]
+        expect(user_response['phone-number']).to eql valid_attributes['phone-number']
+      end
+
+      it { expect(response).to have_http_status(:ok) }
+
+      context 'with unauthenticated user' do
+        before(:each) do
+          put api_v1_user_path(users.first.id), params: { user: valid_attributes }
+        end
+
+        it { expect(response).to have_http_status(:unauthorized) }
+      end
+    end
+
+    context 'with invalid attributes' do
+      before(:each) do
+        put api_v1_user_path(users.first.id),
+            headers: auth_headers(users.first.id),
+            params: { user: invalid_attributes }
+      end
+
+      it 'response contains the correct error message' do
+        error_response = json_response[:phone_number]
+
+        expect(error_response).to include "can't be blank"
+      end
+
+      it { expect(response).to have_http_status(:unprocessable_entity) }
+    end
+  end
+
+  describe 'DELETE /api/v1/users' do
+    before(:each) do
+      delete api_v1_user_path(users.first.id),
+           headers: auth_headers(users.first.id)
+    end
+
+    it { expect(response).to have_http_status(:no_content) }
+
+    context 'with unauthenticated user' do
+      before(:each) do
+        delete api_v1_user_path(users.first.id)
       end
 
       it { expect(response).to have_http_status(:unauthorized) }
